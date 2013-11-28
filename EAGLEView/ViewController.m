05b4cb09f42e5cd1937d90e01386b42416c47e7a
Jens Willy Johannsen <jens@jenswilly.dk>
2013-11-28 16:01:45 +0100

    Cleanup.

--- EAGLEView/ViewController.m
+++ EAGLEView/ViewController.m
@@ -30,38 +30,14 @@
 {
     [super viewDidLoad];
 
-//	self.scrollView.zoomScale = 0.3;
-
 	NSError *error = nil;
 	EAGLESchematic *schematic = [EAGLESchematic schematicFromSchematicFile:@"LED_resistor" error:&error];
 
 	[self.schematicView setRelativeZoomFactor:0.5];
 	_lastZoom = 0.5;
 	self.schematicView.schematic = schematic;
-
-//	EAGLESchematicView *schematicView = [[EAGLESchematicView alloc] initWithFrame:self.view.bounds];
-//	schematicView.schematic = schematic;
-//	[self.view addSubview:schematicView];
-
-	/*
-	EAGLELibrary *library = schematic.libraries[ 0 ];
-	EAGLESymbol *symbol = library.symbols[ 0 ];
-	UIImageView *imageView = [[UIImageView alloc] initWithImage:[symbol image]];
-	CGRect frame = imageView.frame;
-	frame.origin = CGPointMake( 400, 0 );
-	imageView.frame = frame;
-	[self.view addSubview:imageView];
-
-
-	library = schematic.libraries[ 1 ];
-	symbol = library.symbols[ 0 ];
-	imageView = [[UIImageView alloc] initWithImage:[symbol image]];
-	frame = imageView.frame;
-	frame.origin = CGPointMake( 400, 400 );
-	imageView.frame = frame;
-	[self.view addSubview:imageView];
-	 */
 }
+
 - (IBAction)handlePinchGesture:(UIPinchGestureRecognizer*)recognizer
 {
 	// Add gesture's zoom to previous zoom
